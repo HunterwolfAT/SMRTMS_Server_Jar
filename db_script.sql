edae4347f39d45e1f10ebc97220c5f1a7500e0dc
Maximilian Siess <hunterwolf0815@gmail.com>
2015-06-08 12:22:49 +0200

    changed db_script.sql

--- db_script.sql
+++ db_script.sql
@@ -11,32 +11,43 @@ CREATE TABLE IF NOT EXISTS User
 	Longitude double,
 	Latitude double,
 	Avatar char(15),
-	IsOnline boolean
+	IsOnline boolean,
+
+	PRIMARY KEY (ID)
 );
     
 CREATE TABLE IF NOT EXISTS User_Friends
-(	Friender_ID char(100),
-	Friendee_ID char(100),
-    Tracking_Flag bool
+(	Friender_ID int,
+	Friendee_ID int,
+    	Tracking_Flag bool,
+
+	FOREIGN KEY (Friender_ID) REFERENCES User(ID),
+	FOREIGN KEY (Friendee_ID) REFERENCES User(ID)
 );
 
-CREATE TABLE IF NOT EXISTS `Event`
+CREATE TABLE IF NOT EXISTS Event
 (
 	ID int unique auto_increment,
-    `Name` char(50),
-    Description text,
-    Position int,
-    Attendees int
+        Name char(50),
+	Description text,
+	Longitude double,
+	Latitude double,
+        Attendees int,
+
+    PRIMARY KEY(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Event_Attendees
 (
 	User_ID int,
-    Event_ID int
+    	Event_ID int,
+
+	FOREIGN KEY (User_ID) REFERENCES User(ID),
+	FOREIGN KEY (Event_ID) REFERENCES Event(ID)
 );
 
 INSERT INTO User
-(Username, Email, `Password`, Longitude, Latitude, Avatar) VALUES
+(Username, Email, Password, Longitude, Latitude, Avatar) VALUES
 ("Hubert", "h@gmail.com", "123456", 13.23, 19.234, "f34tg45gh34.png");
 
-INSERT INTO Event_Attendees (User_ID, Event_ID) VALUES (1, 2);
+#INSERT INTO Event_Attendees (User_ID, Event_ID) VALUES (1, 2);
